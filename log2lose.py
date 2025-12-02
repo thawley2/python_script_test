36fc9cf0805f6ec75ec7923146fbf0fcf6599b62
Thomas Hawley <thawley22@turing.edu>
2025-12-02 16:10:48 -0700

    Add initial data rows to reward.csv

--- log2lose.py
+++ log2lose.py
@@ -4,3 +4,8 @@ import csv
 with open('reward.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['user_id', 'payment'])
+    writer.writerow([1605, 5.0])
+    writer.writerow([1606, 10.0])
+    writer.writerow([1607, 1.0])
+    writer.writerow([1608, 2.0])
+    writer.writerow([1609, 4.0])
