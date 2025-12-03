640de46958049ea450ea43211372c34eae760445
Thomas Hawley <thawley22@turing.edu>
2025-12-03 11:48:21 -0700

    Update reward data in next_reward.csv

--- log2lose.py
+++ log2lose.py
@@ -4,8 +4,4 @@ import csv
 with open('next_reward.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['user_id', 'payment'])
-    writer.writerow([1605, 5.0])
-    writer.writerow([1606, 10.0])
-    writer.writerow([1607, 1.0])
-    writer.writerow([1608, 2.0])
-    writer.writerow([1609, 4.0])
+    writer.writerow([1680, 5.0])
