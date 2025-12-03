83bf1c1ea50f318061efd6a2eb92fb5c8e5c8e28
Thomas Hawley <thawley22@turing.edu>
2025-12-03 12:11:57 -0700

    Fix column header in next_reward.csv from 'payment' to 'reward'

--- log2lose.py
+++ log2lose.py
@@ -3,5 +3,5 @@ import csv
 # Create reward.csv with column headers
 with open('next_reward.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['user_id', 'payment'])
+    writer.writerow(['user_id', 'reward'])
     writer.writerow([1680, 5.0])
